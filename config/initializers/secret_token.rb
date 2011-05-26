d2ea2bbf32ba6282f270c048326f9a353450774a
Ismail CHAIB <ismail@tesobe.com>
2011-05-26 16:49:12 +0200

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitradio::Application.config.secret_token = 'bf948cb9cbe1de0acec5896d0eef0993aa4a3f72997e665d1ed18e1db47a1df20995f25e13274de585e0db4ef436e4f81425517b4e6b0d330e5eddb62b5cab05'
