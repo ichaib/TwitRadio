d1480cba6033deabe4a480c585e60032f61b0388
Ismail CHAIB <i_chaib@esi.dz>
2011-05-28 18:27:02 +0200

    cleaner implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,12 @@ class ApplicationController < ActionController::Base
  
   def refresh
     list = Array.new
-		tweets = getTweets("#nowplaying")
+		hashtag = "#nowplaying"
+		tweets = getTweets(hashtag)
     tweets.each do |tweet|  
-		      query = getSong(tweet)
+		      query = getSong(tweet.text,hashtag)
 					url = getUrl(query)
-				  list.push(updateSong(url, tweet))
+				  list.push(updateSong(url, query))
 		end 
     #list = tweets
 		#list = Song.all
@@ -21,13 +22,12 @@ class ApplicationController < ActionController::Base
   end
 
   def getTweets(query)
-		# Initialize a Twitter search
 		search = Twitter::Search.new
 		search.containing(query).result_type("recent").per_page(10)
 		end
   
-  def getSong(tweet)
-		tweet
+  def getSong(tweet,hashtag)
+		return clean(tweet,hashtag)
   end
 
   def getUrl(query)
@@ -39,8 +39,11 @@ class ApplicationController < ActionController::Base
 		song.title = "Just try to figure it out for the moment"
 		song.url = "will come from soundcloud"
 		song.artist = "someone but not me"
-		song.tweet = tweet.text
+		song.tweet = tweet
 		return song			
   end
 	
+	def clean(str, query)
+		return str.gsub(/#\w*/,"").gsub(/@\w*/,"").gsub(/http.*/,"")
+	end		
 end
